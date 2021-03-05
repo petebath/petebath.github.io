2a60f940c0af0d16285d31b89389f1cf44af5836
petebath <pdbath@gmail.com>
2021-03-05 17:21:33 +0000

    Delete js directory

--- js/colorpicker.js
+++
@@ -1,9 +0,0 @@
-let colorbtn = document.getElementById("color-btn");
-let colordiv = document.getElementById("color-picker");
-
-function fun1() {
-  let colortext = document.getElementById("color-text").value;
-  colordiv.style.backgroundColor = colortext;
-}
-
-colorbtn.addEventListener("click", fun1);
